391b7f798fdf57f2e3e1c1094649198d5d3d49f1
VinhCV <106277523+CVVinh@users.noreply.github.com>
2026-07-13 19:04:17 +0700

    init-source

--- supabase/migrations/20260406025630_eb69d5e6-014b-4877-954d-802a26e588db.sql
+++
@@ -1,9 +0,0 @@
--- Make bucket private
-UPDATE storage.buckets SET public = false WHERE id = 'board-images';
-
--- Add SELECT policy for authenticated users
-CREATE POLICY "Authenticated users can read board images"
-ON storage.objects
-FOR SELECT
-TO authenticated
-USING (bucket_id = 'board-images');
